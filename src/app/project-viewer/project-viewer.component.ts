42153fabd56180991af1d2b6d7ff2032faf12d45
Shubhankar das <shubhankar.das@wingify.com>
2018-11-24 13:10:54 +0530

    revised UI

--- src/app/project-viewer/project-viewer.component.ts
+++ src/app/project-viewer/project-viewer.component.ts
@@ -1,6 +1,6 @@
 import { Title } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
 @Component({
@@ -22,33 +22,45 @@ export class ProjectViewerComponent implements OnInit {
       title: 'VisualGator',
       heroImage: 'assets/images/Editor.png',
       background: '#253756db',
-      class: 'img-shadow pad'
+      imgClass: 'img-shadow',
+      imgContainer: 'pad'
     },
     skillsalpha: {
       id: 'skillsalpha',
       title: 'Skills Alpha',
       heroImage: 'assets/images/cover-desktop.png',
       background: '#283e99',
-      class: 'img-shadow pad'
+      imgClass: 'img-shadow',
+      imgContainer: 'pad'
     },
     yoola: {
       id: 'yoola',
       title: 'Yoola',
       heroImage: 'assets/images/cover.png',
       background: '#f15475',
-      class: 'img-shadow pad'
+      imgClass: 'img-shadow',
+      imgContainer: 'pad'
     }
   };
   project;
   projectName;
 
-  constructor(private titleService: Title, private route: ActivatedRoute, private _location: Location) {}
+  constructor(
+    private titleService: Title,
+    private route: ActivatedRoute,
+    private _location: Location,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.projectName = params.get('projectName');
-      this.titleService.setTitle(this.projectDetails[this.projectName].title);
-      this.project = this.projectDetails[this.projectName];
+      if (this.projectDetails.hasOwnProperty(this.projectName)) {
+        this.titleService.setTitle(this.projectDetails[this.projectName].title);
+        this.project = this.projectDetails[this.projectName];
+      } else {
+        this.router.navigate(['/']);
+      }
     });
   }
 
