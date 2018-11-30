7e66ff5b608c04937b617c08ec19384a81b4d11d
Shubhankar das <shubhankar.das@wingify.com>
2018-11-30 23:00:22 +0530

    before adding fadeIn elements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-home',
@@ -8,78 +7,17 @@ import { Router } from '@angular/router'
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(private titleService: Title, private router: Router) {}
+  constructor(private titleService: Title, public el: ElementRef) {}
 
-  updateMoon(section) {
-    let moon = document.getElementById('moon');
-
-    if (section === 'home') {
-      moon.style.cssText = '';
-      moon.classList.add('moon-init');
-      return;
-    }
-
-    moon.classList.remove('moon-init');
-
-    if (section === 'skills') {
-      moon.style.cssText = `
-      width: 20px;
-      height: 250px;
-      top: 40%;
-      right: 80%;
-      border-radius: 0;
-    `;
-    } else if (section === 'footer') {
-    } else {
-      let anchor = document.getElementById(section);
-
-      console.log(anchor.getAttribute('data-anchor'));
-
-      if (anchor.getAttribute('data-anchor') === 'left') {
-        moon.style.cssText = `
-          width: 50px;
-          height: 50px;
-          top: 30%;
-          right: 80%;
-          border-radius: 50%;`;
-      } else {
-        moon.style.cssText = `
-          width: 50px;
-          height: 50px;
-          top: 30%;
-          right: 15%;
-          border-radius: 50%;`;
-      }
-    }
-  }
-
-  updateBar(section) {
-    let sidebar = document.getElementById('side-bar');
-
-    let selectedTab = Array.from(sidebar.querySelectorAll('.selected'));
-    selectedTab.forEach(li => li.classList.remove('selected'));
-
-    if (section === 'home') {
-      sidebar.classList.add('hide-slide');
-    } else {
-      sidebar.classList.remove('hide-slide');
-    }
-
-    let activeSideBar = Array.from(
-      sidebar.querySelectorAll(`li[data-section-anchor='${section}']`)
-    );
-    activeSideBar.forEach(li => li.classList.add('selected'));
-  }
-
-  updateSection(section) {
-    let scrollTo = document.getElementById(section);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
-
-    this.updateBar(section);
-    this.updateMoon(section);
-  }
   ngOnInit() {
     this.titleService.setTitle('Shubhankar Das');
-    console.log(this.router.url)
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.el.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset;
+
+    console.log(componentPosition + ' ' + scrollPosition );
   }
 }
